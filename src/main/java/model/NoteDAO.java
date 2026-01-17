dedc617c67390d33a2ccfb926cda1477fb8e3d65
Huynh Huu Phuc <huynhhuuphuc3326@gmail.com>
2026-01-17 18:29:00 +0700

    Complete DAO functions

--- src/main/java/model/NoteDAO.java
+++ src/main/java/model/NoteDAO.java
@@ -1,37 +1,189 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NoteDAO implements DAOInterface<Note> {
 
 	@Override
-	public Note selectById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Note selectById(String idFind) {
+		UserDAO userDAO = new UserDAO();
+		Note note = null;
+		try {
+			// Tao ket noi
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "SELECT * FROM notes WHERE id = ?;";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, idFind);
+			
+			// Thuc thi Statement
+			ResultSet rs = st.executeQuery();
+			
+			// Xu li du lieu
+			if (rs.next()) {
+				note = new Note();
+				note.setId(rs.getString("id"));
+				note.setUser(userDAO.selectById(rs.getString("userid")));
+				note.setTitle(rs.getString("title"));
+				note.setContent(rs.getString("content"));
+				note.setCreateDate(rs.getDate("createdate"));
+				note.setLastEditDate(rs.getDate("lastEditDate"));
+			}
+			
+			// Ngat ket noi database
+			JDBCUtil.closeConnection(c);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return note;
 	}
 
 	@Override
 	public List<Note> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		UserDAO userDAO = new UserDAO();
+		List<Note> result = new ArrayList<>();
+		
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "SELECT * FROM notes;";
+			PreparedStatement st = c.prepareStatement(sql);
+			
+			// Thuc thi Statement
+			ResultSet rs = st.executeQuery();
+			
+			// Xu li ket qua
+			while (rs.next()) {
+				Note note = new Note();
+				note.setId(rs.getString("id"));
+				note.setUser(userDAO.selectById(rs.getString("userid")));
+				note.setTitle(rs.getString("title"));
+				note.setContent(rs.getString("content"));
+				note.setCreateDate(rs.getDate("createdate"));
+				note.setLastEditDate(rs.getDate("lastEditDate"));
+				result.add(note);
+			}
+			
+			// Ngat ket noi database
+			JDBCUtil.closeConnection(c);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public boolean insert(Note t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "INSERT INTO notes (id, user, title, content, createdate, lasteditdate) "
+					+ "VALUES (?, ?, ?, ?, ?, ?);";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, t.getId());
+			st.setString(2, t.getUser().getId());
+			st.setString(3, t.getTitle());
+			st.setString(4, t.getContent());
+			st.setDate(5, t.getCreateDate());
+			st.setDate(6, t.getLastEditDate());
+			
+			// Thuc thi Statement
+			int result = st.executeUpdate();
+			
+			// Dong ket noi
+			JDBCUtil.closeConnection(c);
+			
+			// Xu li ket qua
+			if (result > 0) {
+	            System.out.println("Insert successfully!");
+	            return true;
+	        } else {
+	            System.out.println("Insert failed: No rows affected.");
+	            return false;
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean update(Note t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "UPDATE notes "
+					+ "SET userid = ?, title = ?, content = ?, createdate = ?, lasteditdate = ? "
+					+ "WHERE id = ?;";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, t.getUser().getId());
+			st.setString(2, t.getTitle());
+			st.setString(3, t.getContent());
+			st.setDate(4, t.getCreateDate());
+			st.setDate(5, t.getLastEditDate());
+			st.setString(6, t.getId());
+			
+			// Thuc thi Statement
+			int result = st.executeUpdate();
+			
+			// Ngat ket noi
+			JDBCUtil.closeConnection(c);
+			
+			// Xu li ket qua
+			if (result > 0) {
+				System.out.println("Update successfully!");
+				return true;
+			} else {
+				System.out.println("Update failed! No row is affected.");
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean delete(Note t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "DELETE FROM notes "
+					+ "WHERE id = ?;";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, t.getId());
+			
+			// Thuc thi Statement
+			int result = st.executeUpdate();
+			
+			// Ngat ket noi
+			JDBCUtil.closeConnection(c);
+			
+			// Xu li ket qua
+			if (result > 0) {
+				System.out.println("Delete successfully!");
+				return true;
+			} else {
+				System.out.println("Delete failed! No row is affected.");
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-
 }
--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -1,37 +1,187 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements DAOInterface<User> {
 
 	@Override
-	public User selectById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public User selectById(String idFind) {
+		User user = null;
+		try {
+			// Tao ket noi
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "SELECT * FROM users WHERE id = ?;";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, idFind);
+			
+			// Thuc thi Statement
+			ResultSet rs = st.executeQuery();
+			
+			// Xu li du lieu
+			if (rs.next()) {
+				user = new User();
+				user.setId(rs.getString("id"));
+				user.setUserName(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				user.setEmail(rs.getString("email"));
+				user.setDateOfBirth(rs.getDate("dateofbirth"));
+				user.setGender(rs.getString("gender"));
+			}
+			
+			// Ngat ket noi database
+			JDBCUtil.closeConnection(c);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
 	}
 
 	@Override
 	public List<User> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> result = new ArrayList<>();
+		
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "SELECT * FROM users;";
+			PreparedStatement st = c.prepareStatement(sql);
+			
+			// Thuc thi Statement
+			ResultSet rs = st.executeQuery();
+			
+			// Xu li ket qua
+			while (rs.next()) {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setUserName(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				user.setEmail(rs.getString("email"));
+				user.setDateOfBirth(rs.getDate("dateofbirth"));
+				user.setGender(rs.getString("gender"));
+				result.add(user);
+			}
+			
+			// Ngat ket noi database
+			JDBCUtil.closeConnection(c);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public boolean insert(User t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "INSERT INTO users (id, username, password, email, dateofbirth, gender) "
+					+ "VALUES (?, ?, ?, ?, ?, ?);";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, t.getId());
+			st.setString(2, t.getUserName());
+			st.setString(3, t.getPassword());
+			st.setString(4, t.getEmail());
+			st.setDate(5, t.getDateOfBirth());
+			st.setString(6, t.getGender());
+			
+			// Thuc thi Statement
+			int result = st.executeUpdate();
+			
+			// Dong ket noi
+			JDBCUtil.closeConnection(c);
+			
+			// Xu li ket qua
+			if (result > 0) {
+	            System.out.println("Insert successfully!");
+	            return true;
+	        } else {
+	            System.out.println("Insert failed: No rows affected.");
+	            return false;
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean update(User t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "UPDATE users "
+					+ "SET username = ?, password = ?, email = ?, dateofbirth = ?, gender = ? "
+					+ "WHERE id = ?;";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, t.getUserName());
+			st.setString(2, t.getPassword());
+			st.setString(3, t.getEmail());
+			st.setDate(4, t.getDateOfBirth());
+			st.setString(5, t.getGender());
+			st.setString(6, t.getId());
+			
+			// Thuc thi Statement
+			int result = st.executeUpdate();
+			
+			// Ngat ket noi
+			JDBCUtil.closeConnection(c);
+			
+			// Xu li ket qua
+			if (result > 0) {
+				System.out.println("Update successfully!");
+				return true;
+			} else {
+				System.out.println("Update failed! No row is affected.");
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean delete(User t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			// Tao Connection
+			Connection c = JDBCUtil.getConnection();
+			
+			// Tao Statement
+			String sql = "DELETE FROM users "
+					+ "WHERE id = ?;";
+			PreparedStatement st = c.prepareStatement(sql);
+			st.setString(1, t.getId());
+			
+			// Thuc thi Statement
+			int result = st.executeUpdate();
+			
+			// Ngat ket noi
+			JDBCUtil.closeConnection(c);
+			
+			// Xu li ket qua
+			if (result > 0) {
+				System.out.println("Delete successfully!");
+				return true;
+			} else {
+				System.out.println("Delete failed! No row is affected.");
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-
 }
